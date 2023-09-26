68605eba93f98e53e70e077f5e75a6d01c65eef7
¨marilin¨ <sebastianjmarimon@gmail.com>
2023-09-25 19:06:39 -0500

    implementacion dee rutas

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
+
 
 app.get('/tasks', (req, res) => {
   const tasks = [
@@ -33,6 +36,33 @@ const validateHttpMethod = (req, res, next) => {
 app.use(express.json());
 app.use(validateHttpMethod);
 
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (credencialesValidas) {
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.json({ token });
+  } else { res.status(401).json({ error: 'Credenciales incorrectas' });
+}
+});
+
+app.get('/ruta-protegida', (req, res) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ mensaje: 'Ruta protegida: Acceso concedido', usuario: decoded.username });
+  } catch (error) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+});
+
+  
 app.listen(port, () => {
   console.log(`Servidor Express en funcionamiento en el puerto ${port}`);
 });
